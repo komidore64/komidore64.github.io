d2b8c2ff9b28e77e168c2ccee4c726c6c26310d3
Adam Price <komidore64@gmail.com>
2016-11-18 13:19:58 -0500

    add scores.rb plugin

---
+++ _plugins/score.rb
@@ -0,0 +1,29 @@
+# score.rb
+#
+# Copyright (C) M. Adam Price 2016
+#
+# usage: {% score number %}
+#
+require 'shellwords'
+require 'active_support'
+
+module Jekyll
+
+  class Score < Liquid::Tag
+    def initialize(_, argv, _)
+      super
+
+      argv = Shellwords.split(argv)
+
+      @number = argv.pop
+      raise 'no number given' unless @number
+    end
+
+    def render(_)
+      ::ActiveSupport::NumberHelper.number_to_delimited(@number)
+    end
+  end
+
+end
+
+Liquid::Template.register_tag('score', Jekyll::Score)
--- scores.md
+++ scores.md
@@ -5,7 +5,7 @@ permalink: /high-scores
 ---
 ## [Do-Don-Pachi Resurrection (PC)](http://www.caveshmups.com/dodonpachi)
 
-- 2016.11.12 : 1.5 Novice : 8,923,630,799 ALL - A-Strong
+- 2016.11.12 : 1.5 Novice : {% score 8923630799 %} ALL - A-Strong
 
 ## [Blue Revolver (PC)](http://bluerevolvergame.com/)
 
